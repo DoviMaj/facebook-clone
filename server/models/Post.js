56eec0cfb14c48c48ee2d4530d461941738069a0
dovimaj <dovimajowka@gmail.com>
2021-02-16 23:46:42 +0200

    Post Schema

---
+++ server/models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  User: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  text: {
+    type: String,
+    required: true,
+  },
+  likes: { type: Number, default: 0 },
+  comments: [
+    {
+      User: { type: Schema.Types.ObjectId, ref: "User", required: true },
+      text: { type: String, required: true },
+      likes: { type: Number, default: 0 },
+      date: { type: Date, default: Date.now() },
+    },
+  ],
+  date: { type: Date, default: Date.now() },
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
