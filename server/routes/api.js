0fc384c6a97aef2226f1edc630630f7775130b8b
dovimaj <dovimajowka@gmail.com>
2021-02-16 23:47:48 +0200

    Post routes for Post, Comments and Likes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,102 @@
 const express = require("express");
+const Post = require("../models/Post");
+const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn("/");
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("Hello from Express");
 });
+
+//  Users can create posts.
+router.post("/users/:id/posts", [
+  body("text", "Text can't be empty").not().isEmpty().escape(),
+
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        data: req.body,
+        errors: errors.array(),
+      });
+    }
+    const { text } = req.body;
+    const post = new Post({
+      User: req.params.id,
+      text,
+    });
+    post.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({ msg: "post created" });
+    });
+  },
+]);
+
+// Get users posts || CHANGE FOR TIMELINE
+router.get("/users/:id/posts", async function (req, res, next) {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user !== null) {
+      const posts = await Post.find({ User: req.params.id });
+      if (posts !== null) {
+        // actual success
+        return res.status(200).json({ posts });
+      }
+      return res.status(404).json({ msg: "Posts not found" });
+    }
+    return res.status(404).json({ msg: "User not found" });
+  } catch (err) {
+    res.status(404).json({ msg: err });
+  }
+});
+
+// Like Post
+router.post("/posts/:postId/likes/", async (req, res) => {
+  const post = await Post.findByIdAndUpdate(
+    req.params.postId,
+    { $inc: { likes: 1 } },
+    { new: true }
+  );
+  return res.status(200).json({
+    post: `post with id of ${req.params.postId} has now ${post.likes} likes`,
+  });
+});
+
+// Commment Post
+router.post("/users/:userId/posts/:postId/comments/", [
+  body("text", "Text can't be empty").not().isEmpty().escape(),
+
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        data: req.body,
+        errors: errors.array(),
+      });
+    }
+    const { text } = req.body;
+    const post = await Post.findById(req.params.postId);
+
+    const newComment = { User: req.params.userId, text };
+    post.comments = [...post.comments, newComment];
+    await post.save();
+
+    return res.status(200).json({
+      post,
+    });
+  },
+]);
+
+// Send friend request
+router.get("/users/:sentid/request/:recieverid", function (req, res, next) {
+  res.send("Hello from Express");
+});
+
+// router.get("/users/:sentid/request/:recieverid", function (req, res, next) {
+//   res.send("Hello from Express");
+// });
 
 module.exports = router;
