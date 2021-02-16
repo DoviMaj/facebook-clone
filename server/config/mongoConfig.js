6689cbd386f1da2b62d3ae08f0352aba6d30f730
dovimaj <dovimajowka@gmail.com>
2021-02-16 23:46:29 +0200

    Mongo config and Mongo test config

---
+++ server/config/mongoConfig.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const mongoDB = process.env.MONGOURI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
