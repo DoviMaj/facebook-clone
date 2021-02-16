e812568901e374b1688500479a2b04c8b7f552fb
dovimaj <dovimajowka@gmail.com>
2021-02-16 23:48:37 +0200

    Initial routes

--- server/app.js
+++ server/app.js
@@ -3,24 +3,47 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const apiRouter = require("./routes/api");
+require("dotenv").config();
+require("./config/mongoConfig");
+const passport = require("./config/authConfig");
 
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(
+  require("express-session")({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/", indexRouter);
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get(
+  "/",
+  express.static(path.join(__dirname, "../client/build/index.html"))
+);
+
+app.get("/test", async (req, res) => {
+  res.json({ message: "pass!" });
+});
+
+const apiRouter = require("./routes/api");
+const authRouter = require("./routes/auth");
 app.use("/api", apiRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
