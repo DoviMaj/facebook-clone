2701a36226a47e2f6907358af8bdbf68bb54f11b
dovimaj <dovimajowka@gmail.com>
2021-02-24 22:35:13 +0200

    Cors and session checking route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const createError = require("http-errors");
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
 require("./config/mongoConfig");
+const cors = require("cors");
 const passport = require("./config/authConfig");
 
 const app = express();
@@ -13,12 +15,13 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.SECRET));
 app.use(
-  require("express-session")({
+  session({
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
@@ -32,6 +35,14 @@ app.use(passport.session());
 
 app.get("/test", async (req, res) => {
   res.json({ message: "pass!" });
+});
+
+app.get("/session", (req, res) => {
+  if (req.user) {
+    res.status(200).json({ msg: "user authenticated" });
+  } else {
+    res.status(200).json(null);
+  }
 });
 
 const apiRouter = require("./routes/api");
