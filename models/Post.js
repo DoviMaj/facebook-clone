b98d0bc4d04d1947d6a7510d08bc07ff8a15fce3
dovimaj <dovimajowka@gmail.com>
2021-02-28 20:04:39 +0200

    Date default fixed to function

--- models/Post.js
+++ models/Post.js
@@ -13,10 +13,10 @@ const PostSchema = new Schema({
       User: { type: Schema.Types.ObjectId, ref: "User", required: true },
       text: { type: String, required: true },
       likes: { type: Number, default: 0 },
-      date: { type: Date, default: Date.now() },
+      date: { type: Date, default: Date.now },
     },
   ],
-  date: { type: Date, default: Date.now() },
+  date: { type: Date, default: Date.now },
 });
 
 const Post = mongoose.model("Post", PostSchema);
