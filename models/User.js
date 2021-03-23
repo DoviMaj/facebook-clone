9d543b0c1ed34bf2fe40dd3647750ba8f2a6cf44
dovimaj <dovimajowka@gmail.com>
2021-03-23 02:41:33 +0200

    Add myself as default friend

--- models/User.js
+++ models/User.js
@@ -7,7 +7,12 @@ const UserSchema = new Schema({
   picture_url: { type: String },
   facebookId: { type: String },
   googleId: { type: String },
-  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
   friendsRequestsSent: [{ type: Schema.Types.ObjectId, ref: "User" }],
   friendsRequestsRecieved: [{ type: Schema.Types.ObjectId, ref: "User" }],
   chats: [
@@ -17,11 +22,25 @@ const UserSchema = new Schema({
         ref: "Chat",
       },
       to: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+        type: String,
       },
     },
   ],
+});
+
+UserSchema.pre("save", function (next) {
+  console.log(
+    this._id,
+    this._id !== "6059381854200dbb9b78b8ad",
+    !this.friends.includes("6059381854200dbb9b78b8ad")
+  );
+  if (
+    this._id !== "6059381854200dbb9b78b8ad" &&
+    !this.friends.includes("6059381854200dbb9b78b8ad")
+  ) {
+    this.friends.push("6059381854200dbb9b78b8ad");
+  }
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
